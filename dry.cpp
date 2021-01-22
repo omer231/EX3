4b0627ce08cd16c597e70265129b12437bcd8605
imaymine <75173130+imaymine@users.noreply.github.com>
2021-01-22 14:02:49 +0200

    Update dry.cpp

--- dry.cpp
+++ dry.cpp
@@ -6,13 +6,13 @@ template<class T>
 std::vector<T> slice(std::vector<T> vec, int start, int step, int stop)
 {
     if (start < 0 || start >= vec.size() || stop < 0 || stop > vec.size() || step <= 0)
-	{
+    {
         throw BadInput();
     }
     std::vector<T> newVec;
 
     for (int i = start; i < stop; i += step)
-	{
+    {
         T value;
         value = vec[i];
         newVec.push_back(value);
